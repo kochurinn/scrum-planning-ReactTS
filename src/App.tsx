1419386a436300ffb660cbb954b7234fbffd1684
kochurinn <kochurinn@yandex.ru>
2024-05-22 22:57:42 +0500

    scrum-planning
    scrum-planning

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modalStatus, setModalStatus] = useState(false)
+
+  const onClickAddPlayer = () => {
+    setModalStatus(true)
+  }
+
+  const onClickCloseModal = () => {
+    setModalStatus(false)
+  }
 
   return (
     <>
       <div>
+        <div 
+          className="add-player"
+          onClick={onClickAddPlayer}
+          >
+            Add player
+        </div>
+
+        <div className={`overlap ${modalStatus ? '' : 'd-none'}`}>
+          <div className="modal">
+            <div className="modal__close" onClick={onClickCloseModal}></div>
+            <div className="modal__title">Добавить игрока</div>
+            <input type="text" placeholder='Введите имя игрока' />
+            <div className="modal__btn">Добавить</div>
+          </div>
+        </div>
         
+
       </div>
     </>
   )
